04a64fcfc80d86a1447f9f818da29151edb6e382
RandomNari <giotareddragon@gmail.com>
2026-05-19 23:19:38 +0200

    change whats in the brackets and add a =.

--- mixin/PlayerMixin.java
+++ mixin/PlayerMixin.java
@@ -1,9 +1,9 @@
 @Mixin(Player.class)
 public abstract class PlayerMixin{
 @Inject(method = "attack", at = @At("TAIL"))
-    public void fire_aspect_torch(final Entity entity){
+    public void fire_aspect_torch(Entity entity, CallbackInfo ci){
     if (entity instanceof LivingEntity)
-        if (attackingItemStack=Items.TORCH)
+        if (attackingItemStack== =Items.TORCH)
         {
         onFire(entity) = true;
         }
