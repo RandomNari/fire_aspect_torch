cd001cbabe1be783848c707c2d10e86c015d44f3
RandomNari <giotareddragon@gmail.com>
2026-05-19 23:23:03 +0200

    copied the get weaponItem from the player.class so that the mixin knows the item if this was the problem

--- mixin/PlayerMixin.java
+++ mixin/PlayerMixin.java
@@ -3,6 +3,7 @@ public abstract class PlayerMixin{
 @Inject(method = "attack", at = @At("TAIL"))
     public void fire_aspect_torch(Entity entity, CallbackInfo ci){
     if (entity instanceof LivingEntity)
+        ItemStack attackingItemStack = this.getWeaponItem();
         if (attackingItemStack==Items.TORCH)
         {
         onFire(entity) = true;
