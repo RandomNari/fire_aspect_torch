d3b2924d40c2350fe1d81845864223ff9cf18b78
RandomNari <giotareddragon@gmail.com>
2026-05-19 23:12:31 +0200

    fixing it to TAIL and removing the static

--- mixin/PlayerMixin.java
+++ mixin/PlayerMixin.java
@@ -1,7 +1,7 @@
 @Mixin(Player.class)
 public abstract class PlayerMixin{
-@inject(method = "attack", at = @At("Tail"))
-    public static void fire_aspect_torch(final Entity entity){
+@inject(method = "attack", at = @At("TAIL"))
+    public void fire_aspect_torch(final Entity entity){
     if (entity instanceof LivingEntity)
         if (attackingItemStack=Items.TORCH)
         {
