8bce194c1d8ef18e8b66d7f67383bd6d6dd67cc2
RandomNari <giotareddragon@gmail.com>
2026-05-19 23:14:15 +0200

    i to I

--- mixin/PlayerMixin.java
+++ mixin/PlayerMixin.java
@@ -1,6 +1,6 @@
 @Mixin(Player.class)
 public abstract class PlayerMixin{
-@inject(method = "attack", at = @At("TAIL"))
+@Inject(method = "attack", at = @At("TAIL"))
     public void fire_aspect_torch(final Entity entity){
     if (entity instanceof LivingEntity)
         if (attackingItemStack=Items.TORCH)
