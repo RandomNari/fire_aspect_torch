9e05fcb0feec2ade742aec643814cc69e706a485
RandomNari <giotareddragon@gmail.com>
2026-05-19 23:01:39 +0200

    making the if clauses with the help from the attack method and changing Head to Tail. i also added (final Entity entity) which was also in attack

--- mixin/PlayerMixin.java
+++ mixin/PlayerMixin.java
@@ -1,4 +1,12 @@
 @Mixin(Player.class)
 public abstract class PlayerMixin{
-@inject(method = "attack", at = @At("Head"))
+@inject(method = "attack", at = @At("Tail"))
+    public static void fire_aspect_torch(final Entity entity){
+    if (entity instanceof LivingEntity)
+        if (attackingItemStack=Items.TORCH)
+        {
+
+        }
+
+    }
 }
