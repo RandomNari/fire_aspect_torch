d79444121e19b1ced58d50644fb25cc5daff8bb3
RandomNari <giotareddragon@gmail.com>
2026-05-22 00:14:25 +0200

    it finally works  T-T i didnt know you were supposed to import things but then i hovered with my cursor above the red words and i they were automatically imported and now it works :D (after removing the Living entity if clause that broke my code but i actually think its cool that you can make boats and armor stand burn since thats not too unrealistic. but maybe ill try to make it a configurable setting if i can figure out how to do configurable settings and how to fix the living entity if clause problem

--- src/main/java/random/nari/fire_aspect_torch/mixin/PlayerMixin.java
+++ src/main/java/random/nari/fire_aspect_torch/mixin/PlayerMixin.java
@@ -1,19 +1,28 @@
 package random.nari.fire_aspect_torch.mixin;
 
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.world.entity.Entity;
+
+
 @Mixin(Player.class)
 public abstract class PlayerMixin{
 
-    @Shadow public abstract ItemStack getWeaponItem();
+    @Shadow
+    public abstract ItemStack getWeaponItem();
     @Inject(method = "attack", at = @At("TAIL"))
     public void fire_aspect_torch(Entity entity, CallbackInfo ci) {
-        System.out.println("1 works");
-    if (entity instanceof LivingEntity theEntity) {
-        System.out.println("2 works");
         ItemStack attackingItemStack = this.getWeaponItem();
         if (attackingItemStack.is(Items.TORCH)) {
-            theEntity.igniteForSeconds(4);
-            System.out.println("3 works");
+            entity.igniteForSeconds(4);
         }
 
     }
-}}
+}
