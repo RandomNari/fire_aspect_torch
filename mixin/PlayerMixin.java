2bd77f09f6a76ae7144a86cc10c544b542ff2cca
RandomNari <giotareddragon@gmail.com>
2026-05-21 18:46:39 +0200

    systemoutprintln for seeing where the problem is

--- mixin/PlayerMixin.java
+++ mixin/PlayerMixin.java
@@ -3,12 +3,13 @@ public abstract class PlayerMixin{
     @Shadow public abstract ItemStack getWeaponItem();
     @Inject(method = "attack", at = @At("TAIL"))
     public void fire_aspect_torch(Entity entity, CallbackInfo ci) {
-
+        System.out.println("1 works");
     if (entity instanceof LivingEntity theEntity) {
-
+        System.out.println("2 works");
         ItemStack attackingItemStack = this.getWeaponItem();
         if (attackingItemStack.is(Items.TORCH)) {
             theEntity.igniteForSeconds(4);
+            System.out.println("3 works");
         }
 
     }
