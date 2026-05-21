f02e40cca9e839f086a343c419dc7b414d5eb727
RandomNari <giotareddragon@gmail.com>
2026-05-21 18:08:17 +0200

    trying it with @shadow i really dont know what else to try

--- mixin/PlayerMixin.java
+++ mixin/PlayerMixin.java
@@ -3,8 +3,7 @@ public abstract class PlayerMixin{
 @Inject(method = "attack", at = @At("TAIL"))
     public void fire_aspect_torch(Entity entity, CallbackInfo ci) {
 
-    @Shadow
-    public abstract ItemStack getWeaponItem();
+    @Shadow public abstract ItemStack getWeaponItem();
 
     if (entity instanceof LivingEntity) {
     }
